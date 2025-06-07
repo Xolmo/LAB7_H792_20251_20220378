59f576b7a4bbefa989bd45dde9bb5b7eebee8974
Xolmo <a20220378@pucp.edu.pe>
2025-06-06 20:04:51 -0500

    Initial Commit

---
+++ src/main/java/com/example/lab7_20220378/Lab720220378Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20220378;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720220378Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab720220378Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab7_20220378/Lab720220378ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20220378;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab720220378ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
